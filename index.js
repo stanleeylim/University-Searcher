914e060fab68efe0f0f08f430cbe9548dc407ea6
Stanley Limonthy <stanley.limonthy@binus.edu>
2021-10-08 11:00:57 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+var univList = []
+
+async function pushUniversitiesList()
+{
+    let list = document.getElementById("universities-list");
+    
+    await getUniversities().then(data => {
+        for (const key in data) {
+            univList.push(data[key].name)
+        }
+    })
+
+    univList.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        li.classList.add("list-group-item");
+        list.appendChild(li);
+    });
+
+}
+
+function getUniversities()
+{
+    let data = fetch("http://universities.hipolabs.com/search?country=Indonesia")
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    return data;
+}
+
+function searchUniversities()
+{   
+    let searchList = []
+    input = document.getElementsByTagName("input")[0].value;
+
+    searchList = univList.filter((item) => {
+       return item.toLowerCase().includes(input.toLowerCase())
+    })  
+
+    // console.log(searchList)
+    return updateUniversitiesList(searchList);
+}
+
+function updateUniversitiesList(searchList)
+{
+    let list = document.getElementById("universities-list");
+
+    let child = list.lastElementChild;
+
+    while (child) {
+        list.removeChild(child);
+        child = list.lastElementChild;
+    }
+
+    searchList.forEach(item => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        li.classList.add("list-group-item");
+        list.appendChild(li);
+    });
+}
+
+
